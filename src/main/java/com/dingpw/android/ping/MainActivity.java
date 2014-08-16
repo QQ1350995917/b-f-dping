3248074111ef7072d6558e2663c69dcc423a9f98
QQ1350995917 <www.dingpengwei@gmail.com>
2014-08-16 14:55:16 +0800

    add about submodule

--- src/main/java/com/dingpw/android/ping/MainActivity.java
+++ src/main/java/com/dingpw/android/ping/MainActivity.java
@@ -1,18 +1,25 @@
 package com.dingpw.android.ping;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import com.dingpw.android.setting.about.AboutView;
 
 
 public class MainActivity extends Activity {
 
+    private AlertDialog alertDialog = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new PingView(this));
+        this.setContentView(new PingView(this));
+        this.alertDialog = new AlertDialog.Builder(this).create();
 
         WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         if (!wifiManager.isWifiEnabled()) {
@@ -32,5 +39,22 @@ public class MainActivity extends Activity {
                 ( i >> 24 & 0xFF) ;
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(this.alertDialog.isShowing()){
+            this.alertDialog.dismiss();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_MENU){
+            this.alertDialog.show();
+            Window window = this.alertDialog.getWindow();
+            window.setContentView(new AboutView(this));
+        }
+        return super.onKeyUp(keyCode, event);
+    }
 }
